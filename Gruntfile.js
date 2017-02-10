5bdd113e3577f48c73d033429fefebf0590a4536
TheBoy <theboy5140@gmail.com>
2017-02-10 15:36:14 +0800

    config grunt

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+/**
+ * PROJECT: MyNes
+ * AUTHOR :　TheBoy <theboy5140@gmail.com>
+ * DATE : 2017/2/10 15:19
+ */
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/*.js'],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/* project: <%= pkg.name %>, author: <%= pkg.author%>, contact: <%=pkg.email %>, date: <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'src/*.js'],
+            options: {
+                globals: {
+                    jQuery: false
+                }
+            }
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['jshint','concat', 'uglify']);
+
+};
---
+++ dist/mynes.js
@@ -0,0 +1,25 @@
+/**
+ * PROJECT: MyNes
+ * AUTHOR :　TheBoy <theboy5140@gmail.com>
+ * DATE : 2017/2/9 16:30
+ */
+;/**
+ * PROJECT: MyNes
+ * AUTHOR :　TheBoy <theboy5140@gmail.com>
+ * DATE : 2017/2/9 16:30
+ */
+;/**
+ * PROJECT: MyNes
+ * AUTHOR :　TheBoy <theboy5140@gmail.com>
+ * DATE : 2017/2/9 15:27
+ */
+;/**
+ * PROJECT: MyNes
+ * AUTHOR :　TheBoy <theboy5140@gmail.com>
+ * DATE : 2017/2/9 16:27
+ */
+;/**
+ * PROJECT: MyNes
+ * AUTHOR :　TheBoy <theboy5140@gmail.com>
+ * DATE : 2017/2/9 16:30
+ */
